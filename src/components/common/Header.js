ec987885a30a2e9cd19fff0bf244e5014a2cab13
jinii9 <jinii915@naver.com>
2023-10-31 14:38:48 +0900

    header 스타일 변경

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import {ReactComponent as Logo} from '../../assets/images/Logo.svg';
 import Profile from '../../assets/images/Profile.jpeg';
-import { S_bold_25, XS_bold_13 } from '../style/Styled';
+import { Header_font, S_bold_25, XS_bold_13 } from '../style/Styled';
 import { Link } from 'react-router-dom';
+import Video from './Video';
 
 
 const Header = () => {
@@ -16,11 +17,11 @@ const Header = () => {
             {/* 로그인 O */}
             <MenuWrap>
                 <Menu>
-                    <MenuL to={'/subscribed'}>Subscribed</MenuL>
+                    <MenuL to={'/subscribed'}>구독</MenuL>
                     <Alarm>10</Alarm>
                 </Menu>
                 <Menu>
-                    <MenuL to={'/notification'}>Notification</MenuL>
+                    <MenuL to={'/notification'}>알림</MenuL>
                     <Alarm>21</Alarm>
                 </Menu>
 
@@ -32,6 +33,7 @@ const Header = () => {
                         <Title>Hani Tech World</Title>
                         <Name>@hanitech</Name>
                     </TitleWrap>
+                    {/* <Video/> */}
                 </Menu>
             </MenuWrap>
         </Wrap>
@@ -42,7 +44,7 @@ export default Header;
 
 const Wrap = styled.div`
     background: var(--white);
-    padding: 1.88rem 6.25rem;    
+    padding: 1.2rem 6.25rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -53,10 +55,10 @@ const Wrap = styled.div`
 const LogoL = styled(Link)``;
 const MenuWrap = styled.div`
     display: flex;
-    gap: 3.62rem;
+    gap: 2.5rem;
     align-items: center;
 `;
-const Menu = styled(S_bold_25)`
+const Menu = styled(Header_font)`
     display: flex;
     align-items: center;
 `;
@@ -65,8 +67,8 @@ const MenuL = styled(Link)`
     color: var(--black);
 `;
 const ProfileImg = styled.div`
-    width: 3.375rem;
-    height: 3.375rem;
+    width: 2.5rem;
+    height: 2.5rem;
     border-radius: 2.5rem;
     background: url(${Profile}) lightgray 50% / cover no-repeat;
     margin-right: 0.94rem;
@@ -86,7 +88,7 @@ const Name = styled.div`
     color: var(--gray_bold, #4A4A4A);
     text-align: justify;
     font-family: Pretendard;
-    font-size: 1.125rem;
+    font-size: 1rem;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
