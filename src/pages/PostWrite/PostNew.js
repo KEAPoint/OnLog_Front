27b4a280f6fa1839692bf1db00e7e955beb16b72
Jiyeon <yougi8@naver.com>
2023-11-12 13:44:24 +0900

    글 작성 페이지1

--- src/pages/PostWrite/PostNew.js
+++ src/pages/PostWrite/PostNew.js
@@ -1,65 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SunEditor from "suneditor-react";
 import _ from 'lodash';
 import "suneditor/dist/css/suneditor.min.css";
 import styled from "styled-components";
 import TextareaAutosize from 'react-textarea-autosize'; // npm install react-textarea-autosize
-import { S_bold_25 } from "../../components/style/Styled";
+import { S_bold_19_2, XS_bold_13} from "../../components/style/Styled";
 import Swal from "sweetalert2";
+import { Get_Categori } from "../../apis/API_MyPage";
+import { useNavigate } from "react-router-dom";
 
 
 const PostNew = () => {
+    const nagivate = useNavigate();
     const [content, setContent] = useState("");
-    const [submittedContent, setSubmittedContent] = useState("");
     const [title, setTitle] = useState("");
+    const [topic, setTopic] = useState("");
     const [category, setCategory] = useState("");
-    const [tag, setTag] = useState("");
 
-    const categories = ["lifestyle", "travel", "foodie", "entertainment", "tech", "sports", "news"]; // 카테고리 리스트
+    const topics = ["lifestyle", "travel", "foodie", "entertainment", "tech", "sports", "news"]; // 주제 리스트
 
     // onChange 발생 시 content에 저장 (디바운스 적용)
     const handleChange = _.debounce((newContent) => {
         setContent(newContent);
         console.log(newContent);
-    }, 300); // 300ms 딜레이
-
-   // 제출 버튼 클릭 시 content의 현재 내용을 submittedContent에 저장
-   const handleSubmit = () => {
-       setSubmittedContent(content);
-       console.log(submittedContent);
-    //    alert(category);
-   }
+    });
 
    // 제목 입력시 변경사항 반영
    const TitleHandler = (e) => {
     setTitle(e.currentTarget.value);
     }
 
-    const TagHandler = (e) => {
-        setTag(e.currentTarget.value);
-    }
-
     const nextBtnHandler = () => {
         // 제목, 글 내용, 해시태그, 카테고리 모두 선택했는지 확인해야함
+        if(title==""){
+            alert('제목을 입력해주세요');
+            window.scrollTo({top:0, behavior:"smooth"});
 
-        Swal.fire(
-            {
-                title: "...",
-                // icon: "success",
-                text: "AI가 썸네일과 3줄요약을 생성 중입니다! 조금만 기다려주세요"
-            }
-        )
+        }
+        else if (content==""){
+            alert('글을 작성해주세요');
+            window.scrollTo({top:0, behavior:"smooth"});
+        }
+        else{
+            console.log('해시태그: ', tagList);
+            console.log('카테고리 : ',category);
+            console.log('주제 : ',topic);
+            console.log('내용 : ', content);
+
+            // navigate(``);
+        }
     }
+
+    const [tagItem, setTagItem] = useState('')
+    const [tagList, setTagList] = useState([])
+  
+    const onKeyPress = e => {
+      if (e.target.value.length !== 0 && e.key === "Enter") { // enter key 누르면
+        submitTagItem() // tagItem에 저장
+      }
+    }
+    // 태그리스트에 태그아이템 추가
+    const submitTagItem = () => {
+      if (!tagList.includes(tagItem)) { // tagList에 tagItem이 포함되어 있지 않은 경우에만 추가
+          let updatedTagList = [...tagList] // 현재 taglist 복사
+          updatedTagList.push(tagItem) // taglist 끝에 현재 태그아이템 추가
+          setTagList(updatedTagList)
+          setTagItem('')
+      }
+      else{
+          alert('중복값 x')
+      }
+      console.log("tag add. taglist : ", tagList);
+
+    }
+    // 태그 삭제
+    const deleteTagItem = e => {
+      const deleteTagItem = e.target.parentElement.firstChild.innerText
+      const filteredTagList = tagList.filter(tagItem => tagItem !== deleteTagItem)
+      setTagList(filteredTagList)
+      console.log("tag delete. taglist : ", tagList);
+    }
+
+    // 카테고리
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        const fetchCategory = async () => {
+            const data = await Get_Categori();
+            
+            let sortedData = data.data.sort((a, b) => a.order - b.order);
+            console.log("sortedData: ",sortedData);
+            setCategories(sortedData);
+        }
+        fetchCategory();
+      }, []);
+
+
+
+
    return (
     <Wrap>
-        <WrapInner>
-            <S_bold_25>제목</S_bold_25>
-        </WrapInner>
-        <Title type='text' value={title} style={{ overflow: 'hidden' }} placeholder="제목을 입력하세요" onChange={TitleHandler}/>
+        {/* <StickyWrap> */}
+            <Title>제목</Title>
+            <TitleInput type='text' value={title} style={{ overflow: 'hidden' }} placeholder="제목을 입력하세요" onChange={TitleHandler}/>
+        {/* </StickyWrap> */}
 
         <div className="text-editor">
             <SunEditor 
-                width="100rem"
+                width="90rem"
                 height="70rem"
                 setOptions={{
                     buttonList: [
@@ -81,24 +128,57 @@ const PostNew = () => {
                 }}
                 onChange={handleChange}
             />
-            {/* <button onClick={handleSubmit}>제출</button> */}
         </div>
         
-        <WrapInner>
-            <S_bold_25> 카테고리 선택 </S_bold_25>
+        {/* 해시태그 입력 */}
+        <Title>
+            <div> 해시태그 입력 </div>
+            <Tagging> * 해시태그 하나씩 입력 후 엔터를 눌러주세요!</Tagging>
+            <TagBox> 
+                {/* 생성된 태그 하나하나당 박스 */}
+                {tagList.map((tagItem, index) => {
+                return (
+                    <TagItem key={index}>
+                    <Text>{tagItem}</Text>
+                    <XButton onClick={deleteTagItem}>X</XButton>
+                    </TagItem>
+                )
+                })}
+                {/* 태그 입력하는 입력칸 */}
+                <TagInput
+                    type='text'
+                    tabIndex={2}
+                    onChange={e => setTagItem(e.target.value)}
+                    value={tagItem}
+                    onKeyPress={onKeyPress} // enter누르면 해시태그 생성
+                />
+            </TagBox>
+        </Title>
+
+        {/* 카테고리 선택 */}
+        <Title>
+            <div>카테고리 선택</div>
             <div style={{paddingTop:"1rem", paddingBottom:"1rem"}}>
-                {categories.map((cat) => (
-                    <CategoryButton key={cat} onClick={() => setCategory(cat)} selected={category === cat}>
-                        #{cat}
-                    </CategoryButton> 
+                {categories.map((cate) => (
+                    <CateBtn key={cate.id} onClick={() => setCategory(cate.name)} selected={category === cate.name}>
+                        {cate.name}
+                    </CateBtn> 
                 ))}
             </div>
-        </WrapInner>
+        </Title>
 
-        <WrapInner>
-            <S_bold_25> 해시태그 입력 </S_bold_25>
-            <Title type='text' value={tag} style={{ overflow: 'hidden' }} placeholder="해시태그를 입력해보세요" onChange={TagHandler}/>
-        </WrapInner>
+
+        {/* 주제 선택 */}
+        <Title>
+            <div>주제 선택</div>
+            <div style={{paddingTop:"1rem", paddingBottom:"1rem"}}>
+                {topics.map((top) => (
+                    <TopicBtn key={top} onClick={() => setTopic(top)} selected={topic === top}>
+                        #{top}
+                    </TopicBtn> 
+                ))}
+            </div>
+        </Title>
 
         <BtnWrap>
             <NextBtn onClick={nextBtnHandler}>다음</NextBtn>
@@ -110,6 +190,89 @@ const PostNew = () => {
 
 export default PostNew;
 
+
+
+const Tagging = styled(XS_bold_13)`
+    padding:0.5rem 0rem 0rem 0.5rem;
+    color: red;
+`
+const TagBox = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    margin: 1rem 1rem 1rem 0rem;
+    padding: 0.5rem;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+
+    &:focus-within {
+    border-color: #FF7575;
+    }
+
+    &:hover{
+    box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.05);
+    }
+`
+
+const TagItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-right: 1rem;
+    padding: 0rem 0.5rem;
+    background-color: #FF7575;
+    border-radius: 5px;
+    color: white;
+
+    /* S-medium-20.8(RE) */
+    font-family: Pretendard;
+    font-size: 1.3rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 158.023%; /* 2.05431rem */
+  `
+
+const Text = styled.span``
+
+const XButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1rem;
+    height: 1rem;
+    margin-left: 0.2rem;
+    background-color: white;
+    border-radius: 50%;
+    border: 1px solid white;
+    color: tomato;
+
+    cursor: pointer;
+`
+
+const TagInput = styled.input`
+    display: inline-flex;
+    background: transparent;
+    border: none;
+    outline: none;
+    cursor: text;
+
+    /* S-regular_20(RE) */
+    font-family: Pretendard;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 2.3rem; /* 184% */
+    letter-spacing: 0.0125rem;
+`
+
+
+
+const StickyWrap = styled.div`
+    background-color: white;
+    position: sticky;
+    top: 0;
+`
 const Wrap = styled.div`
     display: flex;
     flex-direction: column;
@@ -119,19 +282,18 @@ const Wrap = styled.div`
     align-self: stretch;
 `
 
-const WrapInner = styled.div`
+const Title = styled(S_bold_19_2)`
   width: 100%;
   display: flex;
   align-items: flex-start;
   flex-direction: column;
 `
-const Title = styled(TextareaAutosize)`
+const TitleInput = styled.input`
     width: 100%;
     display: flex;
-    padding: 0.6875rem 1.4375rem;
     align-items: center;
     gap: 0.625rem;
-    align-self: stretch;
+    padding: 1rem 0rem 1rem 1rem;
     border: none;
     border-bottom: 1.5px solid var(--gray_bold, #4A4A4A);
 
@@ -139,24 +301,50 @@ const Title = styled(TextareaAutosize)`
 
     color: var(--gray_bold, #4A4A4A);
 
-    /* S-regular_30(txt) */
+    /* S-regular_20(RE) */
     font-family: Pretendard;
-    font-size: 1.875rem;
+    font-size: 1.25rem;
     font-style: normal;
     font-weight: 400;
-    line-height: 3.125rem; /* 166.667% */
-    letter-spacing: 0.01875rem;
+    line-height: 2.3rem; /* 184% */
+    letter-spacing: 0.0125rem;
 
     &:hover{
         box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.05);
     }
 `
-
-const CategoryButton = styled.button`
+const CateBtn = styled.button`
     color: ${props => props.selected ? 'white' : 'var(--gray_bold, #4A4A4A)'};
     background-color: ${props => props.selected ? '#FF7575' : 'white'};
 
-    /* L-semibold-40 */
+    /* S-bold-19.2(RE) */
+    font-family: Pretendard;
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    border: 1px solid black;
+    padding: 0.75rem 1rem;
+    margin-right: 1rem;
+    cursor: pointer;
+    transition: transform 0.1s ease-in-out;
+
+    &:hover {
+        color: white;
+        background: #FF7575;
+    }
+
+    //버튼 클릭 시 애니메이션
+    &:active {
+        transform: scale(0.95);
+    }
+`
+const TopicBtn = styled.button`
+    color: ${props => props.selected ? 'white' : 'var(--gray_bold, #4A4A4A)'};
+    background-color: ${props => props.selected ? '#FF7575' : 'white'};
+
+    /* L-semibold-32(RE) */
     font-family: Pretendard;
     font-size: 2rem;
     font-style: normal;
@@ -171,7 +359,6 @@ const CategoryButton = styled.button`
     &:hover {
         color: white;
         background: #FF7575;
-        font-size: 2.2rem;
     }
 
     //버튼 클릭 시 애니메이션
