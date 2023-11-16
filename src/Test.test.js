f02df02e1296ac3fb252fdf6977bc1325d9d5d2b
Jiyeon <yougi8@naver.com>
2023-11-16 18:44:14 +0900

    test 코드 추가

--- src/Test.test.js
+++ src/Test.test.js
@@ -1,8 +1,8 @@
-const TestPage = () => {
-    return(
-        <>
-        안녕
-        </>
-    )
-}
-export default TestPage;
+import { render, screen } from '@testing-library/react';
+import TestPage from './TestPage';
+
+test('renders 안녕', () => {
+  render(<TestPage />);
+  const linkElement = screen.getByText(/안녕/i);
+  expect(linkElement).toBeInTheDocument();
+});
